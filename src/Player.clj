e02304f73ffc549fe9e00ac1540e23016670b4bc
Dustin Conrad <duconrad@paypal.com>
2014-11-10 13:59:29 -0500

    Parse input to data structures

--- src/Player.clj
+++ src/Player.clj
@@ -6,7 +6,7 @@
        (re-seq #"\d+")
        (map #(Integer/parseInt %))))
 
-(defn read-platinum-info [zone-count]
+(defn initialize-platinum-info [zone-count]
   (loop [i zone-count
          acc {}]
     (if (zero? i)
@@ -15,7 +15,7 @@
         (dec i)
         (apply assoc acc (read-number-input-line))))))
 
-(defn read-link-info [link-count]
+(defn initialize-link-info [link-count]
   (loop [i link-count
          acc {}]
     (if (zero? i)
@@ -26,15 +26,27 @@
           (-> (update-in acc [zone1] conj zone2)
               (update-in [zone2] conj zone1)))))))
 
+(defn read-round-platinum-info []
+  (->> (read-line)
+       Integer/parseInt))
+
+(defrecord ZoneState [owner-id p0-count p1-count p2-count p3-count])
+
+(defn read-round-game-state [zone-count]
+  (loop [i zone-count
+         acc {}]
+    (if (zero? i)
+      acc
+      (let [[z-id & zone-info] (read-number-input-line)]
+        (recur
+          (dec i)
+          (assoc acc z-id (apply ->ZoneState zone-info)))))))
+
 (defn -main [& args]
   (let [[playerCount my-id zone-count link-count] (read-number-input-line)
-        zone-info (read-platinum-info zone-count)
-        link-info (read-link-info link-count)]
-    ; playerCount: the amount of players (2 to 4)
-    ; myId: my player ID (0, 1, 2 or 3)
-    ; zoneCount: the amount of zones on the map
-    ; linkCount: the amount of links between all zones
-    (binding [*out* *err*]
+        zone-info (initialize-platinum-info zone-count)
+        link-info (initialize-link-info link-count)]
+    #_(binding [*out* *err*]
        (println "zone info")
        (println zone-info)
        (println)
@@ -42,21 +54,13 @@
        (println link-info)
        (println))
     (while true
-      (let [platinum (read)]
-        ; platinum: my available Platinum
-        (loop [i zone-count]
-          (when (> i 0)
-            (let [zId (read) ownerId (read) podsP0 (read) podsP1 (read) podsP2 (read) podsP3 (read)]
-              ; zId: this zone's ID
-              ; ownerId: the player who owns this zone (-1 otherwise)
-              ; podsP0: player 0's PODs on this zone
-              ; podsP1: player 1's PODs on this zone
-              ; podsP2: player 2's PODs on this zone (always 0 for a two player game)
-              ; podsP3: player 3's PODs on this zone (always 0 for a two or three player game)
-              (recur (dec i)))))
+      (let [platinum (read-round-platinum-info)
+            game-state (read-round-game-state zone-count)]
 
-        ; (binding [*out* *err*]
-        ;   (println "Debug messages..."))
+        (binding [*out* *err*]
+          (println "game-state")
+          (println game-state)
+          (println))
 
         ; first line for movement commands, second line for POD purchase (see the protocol in the statement for details)
         (println "WAIT")
